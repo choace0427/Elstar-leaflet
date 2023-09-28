ae06a8c2bc54f7ebf6202492b90f13462df947f8
choace0427 <jacobkevin0427@gmail.com>
2023-09-28 13:23:59 -0400

    fix auth route 3

--- src/components/route/ProtectedRoute.tsx
+++ src/components/route/ProtectedRoute.tsx
@@ -14,7 +14,7 @@ const ProtectedRoute = () => {
         return (
             <Navigate
                 replace
-                to={`${unAuthenticatedEntryPath}?${REDIRECT_URL_KEY}=${location.pathname}`}
+                to={`/home`}
             />
         )
     // }
